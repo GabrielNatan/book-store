88869f61cd45bed34792030a2470a517d18332e9
gabriel natan <g.souza@grouplinknetwork.com>
2022-11-05 21:37:16 -0300

    add form in login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,39 @@
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 import {
     Container,
+    Logo,
+    Form,
+    Tilte,
+    SubTitle,
+
 } from "./styles";
 
 export const Login = ({})=>{
 
     return(
         <Container>
-           Login
+           <Logo>
+
+           </Logo>
+           <Form>
+                <Tilte>Welcome!</Tilte>
+                <SubTitle>
+                    It is a long established fact that a reader will be 
+                    distracted by thereadable content of a page when looking 
+                    at its layout. The point of using Lorem Ipsum is that it.
+                </SubTitle>
+                <Input
+                    type="text"
+                    placeholder="Email"
+                />
+                <Input
+                    type="password"
+                    placeholder="password"
+                />
+                <Button/>
+                <Button/>
+           </Form>
         </Container>
     )
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -3,3 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
 
 `
+export const Logo = styled.div`
+
+`
+
+export const Form = styled.div`
+
+`
+
+export const Tilte = styled.div`
+
+`
+
+export const SubTitle = styled.div`
+
+`
